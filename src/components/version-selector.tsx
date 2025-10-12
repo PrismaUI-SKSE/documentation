4b8621c92a35b5b9e0a437daa6a5f27c1b8cac23
StarkMP <stark.maks.pacs@gmail.com>
2025-10-12 18:06:55 +0300

    docs: add version selector to the download btn

---
+++ src/components/version-selector.tsx
@@ -0,0 +1,73 @@
+import type { ClassNameValue } from 'tailwind-merge';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { cn } from '@/lib/utils';
+
+type Version = {
+  version: string;
+  badge?: {
+    label: string;
+    className: ClassNameValue;
+  };
+  url: string;
+};
+
+const versions: Version[] = [
+  {
+    version: '1.1.0-rc',
+    badge: {
+      label: 'Latest Features',
+      className: 'text-blue-500 border-blue-500',
+    },
+    url: 'https://github.com/PrismaUI-SKSE/framework/releases/download/1.1.0-rc/PrismaUI_1_1_0_rc.zip',
+  },
+  {
+    version: '1.0.0',
+    badge: {
+      label: 'Stable',
+      className: 'text-green-500 border-green-500',
+    },
+    url: 'https://github.com/PrismaUI-SKSE/framework/releases/download/1.0.0/PrismaUI_1_0_0.zip',
+  },
+];
+
+export const VersionSelector = () => (
+  <DropdownMenu modal={false}>
+    <DropdownMenuTrigger asChild>
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a className="cursor-pointer">Download Prisma UI</a>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuLabel>Select the version</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      {versions.map((version) => (
+        <DropdownMenuItem key={version.version} className="cursor-pointer">
+          <div className="flex items-center justify-between w-full gap-3">
+            <div className="flex flex-col gap-0.5">
+              <span className="font-medium">{version.version}</span>
+            </div>
+            {version.badge && (
+              <a
+                href={version.url}
+                target="_blank"
+                className={cn(
+                  `px-2 py-0.5 text-xs font-medium rounded-lg border`,
+                  version.badge.className
+                )}
+              >
+                {version.badge.label}
+              </a>
+            )}
+          </div>
+        </DropdownMenuItem>
+      ))}
+    </DropdownMenuContent>
+  </DropdownMenu>
+);
